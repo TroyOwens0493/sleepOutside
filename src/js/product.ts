39151b50fbadfa994699f5c9dc0498e89977dc63
nathanesky <esc21007@byui.edu>
2026-05-08 23:11:31 -0600

    Fix add to cart functionality

--- src/js/product.ts
+++ src/js/product.ts
@@ -2,8 +2,10 @@ import type {Product} from "./types.mts"
 import { setLocalStorage } from "./utils.mts";
 import { findProductById } from "./productData.mts";
 
-function addProductToCart(product:Product) {
-  setLocalStorage("so-cart", product);
+function addProductToCart(product: Product) {
+  const cart = JSON.parse(localStorage.getItem("so-cart") || "[]");
+  cart.push(product);
+  setLocalStorage("so-cart", cart);
 }
 // add to cart button event handler
 async function addToCartHandler(e:Event) {
