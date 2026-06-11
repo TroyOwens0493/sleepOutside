ecb496c43b3e5d46c3ddb376b96cbf73bd6dfe82
garpunk <garpunked@gmail.com>
2026-06-11 09:48:37 -0600

    added logout

--- src/js/auth.svelte.ts
+++ src/js/auth.svelte.ts
@@ -1,45 +1,53 @@
+import { getLocalStorage, setLocalStorage } from "./utils.mts";
 const BASE_URL = process.env.PUBLIC_SERVER_URL;
 
 interface UserStore {
-    isLoggedIn: boolean,
-    user?: {
-        name: string,
-        email: string,
-        _id: string
-    },
-    token: string
+  isLoggedIn: boolean;
+  user?: {
+    name: string;
+    email: string;
+    _id: string;
+  };
+  token: string;
 }
-export const userStore = $state({ isLoggedIn: false, user: {}, token: '' } as UserStore);
+export const userStore = $state({
+  isLoggedIn: false,
+  user: {},
+  token: "",
+} as UserStore);
 
 export async function login(email: string, password: string) {
-    const loginRes = await fetch(BASE_URL + "users/login", {
-        method: "POST",
-        body: JSON.stringify({
-            email,
-            password,
-        }),
-    });
-    if (!loginRes.ok) {
-        throw new Error("Invalid email or password");
-    }
-    const data = await loginRes.json();
-    console.log("loginData", data);
+  const loginRes = await fetch(BASE_URL + "users/login", {
+    method: "POST",
+    body: JSON.stringify({
+      email,
+      password,
+    }),
+  });
+  if (!loginRes.ok) {
+    throw new Error("Invalid email or password");
+  }
+  const data = await loginRes.json();
+  console.log("loginData", data);
 }
 
-export function logout() { }
+export function logout() {
+  userStore.user = undefined;
+  userStore.token = "";
+  userStore.isLoggedIn = false;
+  setLocalStorage("so-user", null);
+}
 
 export function checkAuth() {
-    const userData = localStorage();
-    if (userData) {
-        userStore.user = userData.user;
-        userStore.token = userData.token;
-        userStore.isLoggedIn = true;    
-    } else {
-        userStore.user = {};
-        userStore.token = '';
-        userStore.isLoggedIn = false;    
-    }
-    return userData;
-
+  const userData = localStorage();
+  if (userData) {
+    userStore.user = userData.user;
+    userStore.token = userData.token;
+    userStore.isLoggedIn = true;
+  } else {
+    userStore.user = {};
+    userStore.token = "";
+    userStore.isLoggedIn = false;
+  }
+  return userData;
 }
-
