f2f0516a6d8e3a047055d873e69f24d531018983
TroyOwens0493 <147565176+TroyOwens0493@users.noreply.github.com>
2026-06-11 10:23:18 -0600

    redirect enhancement

--- src/components/Login.svelte
+++ src/components/Login.svelte
@@ -32,7 +32,14 @@
   onMount(() => {
     // we added the getParam function to utils in Team 5
     const param = getParam("redirect");
-    if (param) redirectPath = param;
+    if (param) {
+      // if the redirect param exists use that
+      redirectPath = param;
+    } else if (document.referrer != window.location.href) {
+      // otherwise send back to the referring page if set
+      redirectPath = document.referrer;
+    }
+    // otherwise redirectPath will stay "/" (send home)
   });
 </script>
 
@@ -84,4 +91,3 @@
     text-align: center;
   }
 </style>
-
