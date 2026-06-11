253d4db41ca1bd2dc88ef1b0414ac351c414c12e
nathanesky <esc21007@byui.edu>
2026-06-11 10:15:16 -0600

    updates

--- src/components/Profile.svelte
+++ src/components/Profile.svelte
@@ -4,19 +4,21 @@
   const baseURL = import.meta.env.PUBLIC_SERVER_URL;
   let profile = $state("");
 
-  async function getProfile() {
+async function getProfile() {
+    console.log(userStore.token);
     const res = await fetch(`${baseURL}users/protected`, {
       method: "GET",
       headers: {
-        
+        // This is how we pass our token to the server for protected routes.
         Authorization: `Bearer ${userStore.token}`
       }
     });
     if (res.ok) {
       return res.json();
     } else {
-    
+      // if we get an error back...it means there is something wrong with our token and we should log the user out.
       logout();
+      window.location.href = `/login/?redirect=${window.location.pathname}`;
     }
   }
   
