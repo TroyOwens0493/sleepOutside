c75669f167b1cab54efae3bc2d913c5957254587
garpunk <garpunked@gmail.com>
2026-06-11 09:34:53 -0600

    random stuff

--- src/js/auth.svelte.ts
+++ src/js/auth.svelte.ts
@@ -1,20 +1,18 @@
 const BASE_URL = process.env.PUBLIC_SERVER_URL;
 
 export async function login(email: string, password: string) {
-    const loginRes = await fetch(BASE_URL + 'users/login',
-        {
-            method: "POST",
-            body: JSON.stringify({
-                email,
-                password
-            })
-        });
-    const data = await loginRes.json();
-    console.log('loginData', data);
+  const loginRes = await fetch(BASE_URL + "users/login", {
+    method: "POST",
+    body: JSON.stringify({
+      email,
+      password,
+    }),
+  });
+  const data = await loginRes.json();
+  console.log("loginData", data);
 }
 
-export function logout() {
-}
+export function logout() {}
 
 export function checkAuth() {
     
