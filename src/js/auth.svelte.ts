36b153ed15b4fcdb9772ee3e7cd83ddb0be165c3
nathanesky <esc21007@byui.edu>
2026-06-11 09:32:18 -0600

    State variable

--- src/js/auth.svelte.ts
+++ src/js/auth.svelte.ts
@@ -5,4 +5,16 @@ export function logout() {
 }
 
 export function checkAuth() {
+    
 }
+
+interface UserStore {
+    isLoggedIn:boolean,
+    user:?{
+        name: string,
+        email: string,
+        _id: string
+    },
+    token: string
+},
+export const userStore = $State( {isLoggedIn: false, user: {}, token: ''} as UserStore);
