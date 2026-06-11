a4d5b1ece9a778bea48071ecc468325fed5fa634
nathanesky <esc21007@byui.edu>
2026-06-11 09:36:44 -0600

    updates

--- src/js/auth.svelte.ts
+++ src/js/auth.svelte.ts
@@ -1,5 +1,16 @@
 const BASE_URL = process.env.PUBLIC_SERVER_URL;
 
+interface UserStore {
+    isLoggedIn:boolean,
+    user:?{
+        name: string,
+        email: string,
+        _id: string
+    },
+    token: string
+},
+export const userStore = $State( {isLoggedIn: false, user: {}, token: ''} as UserStore);
+
 export async function login(email: string, password: string) {
   const loginRes = await fetch(BASE_URL + "users/login", {
     method: "POST",
@@ -18,13 +29,3 @@ export function checkAuth() {
     
 }
 
-interface UserStore {
-    isLoggedIn:boolean,
-    user:?{
-        name: string,
-        email: string,
-        _id: string
-    },
-    token: string
-},
-export const userStore = $State( {isLoggedIn: false, user: {}, token: ''} as UserStore);
