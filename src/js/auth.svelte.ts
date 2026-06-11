73eb7f0b1958aa2d5e7ecd67d1631003ee859d2f
TroyOwens0493 <147565176+TroyOwens0493@users.noreply.github.com>
2026-06-11 09:38:18 -0600

    fix interface definition, throw err on invalid login

--- src/js/auth.svelte.ts
+++ src/js/auth.svelte.ts
@@ -1,31 +1,34 @@
 const BASE_URL = process.env.PUBLIC_SERVER_URL;
 
 interface UserStore {
-    isLoggedIn:boolean,
-    user:?{
+    isLoggedIn: boolean,
+    user?: {
         name: string,
         email: string,
         _id: string
     },
     token: string
-},
-export const userStore = $state( {isLoggedIn: false, user: {}, token: ''} as UserStore);
+}
+export const userStore = $state({ isLoggedIn: false, user: {}, token: '' } as UserStore);
 
 export async function login(email: string, password: string) {
-  const loginRes = await fetch(BASE_URL + "users/login", {
-    method: "POST",
-    body: JSON.stringify({
-      email,
-      password,
-    }),
-  });
-  const data = await loginRes.json();
-  console.log("loginData", data);
+    const loginRes = await fetch(BASE_URL + "users/login", {
+        method: "POST",
+        body: JSON.stringify({
+            email,
+            password,
+        }),
+    });
+    if (!loginRes.ok) {
+        throw new Error("Invalid email or password");
+    }
+    const data = await loginRes.json();
+    console.log("loginData", data);
 }
 
-export function logout() {}
+export function logout() { }
 
 export function checkAuth() {
-    
+
 }
 
